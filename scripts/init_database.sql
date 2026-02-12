dcef7b33c02a6b5b79e216c3a1409efbbc4973a1
Kim Vu Thien <kvthiencre@gmail.com>
2026-02-12 09:10:07 +0700

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -29,120 +29,120 @@ use DataWarehousePJ;
 go
 
 create table bronze.crm_data_video  (
-	vid_creator_info nvarchar(50),
-	vid_creator_id int,
-	vid_video_info nvarchar(50),
-	vid_datetime datetime2(0),
-	vid_prduct nvarchar(50),
-	vid_views int,
-	vid_likes int,
-	vid_cmts int,
-	vid_shares int,
-	vid_followers int,
-	vid_click_watchandfollow int,
-	vid_productimpression int,
-	vid_productclick int,
-	vid_cus int,
-	vid_orders int,
-	vid_orderssold int,
-	vid_gpm int,
-	vid_gmv int,
-	vid_ctr decimal(5,2),
-	vid_click_watchandfollow_ratio decimal(5,2),
-	vid_watchfinished decimal(5,2),
-	vid_clicktoorders decimal(5,2)
+	vid_creator_info nvarchar(max),
+	vid_creator_id nvarchar(max),
+	vid_video_info nvarchar(max),
+	vid_datetime nvarchar(max),
+	vid_prduct nvarchar(max),
+	vid_views nvarchar(max),
+	vid_likes nvarchar(max),
+	vid_cmts nvarchar(max),
+	vid_shares nvarchar(max),
+	vid_followers nvarchar(max),
+	vid_click_watchandfollow nvarchar(max),
+	vid_productimpression nvarchar(max),
+	vid_productclick nvarchar(max),
+	vid_cus nvarchar(max),
+	vid_orders nvarchar(max),
+	vid_orderssold nvarchar(max),
+	vid_gpm nvarchar(max),
+	vid_gmv nvarchar(max),
+	vid_ctr nvarchar(max),
+	vid_click_watchandfollow_ratio nvarchar(max),
+	vid_watchfinished nvarchar(max),
+	vid_clicktoorders nvarchar(max)
 );
 
 create table bronze.crm_data_live (
-	live_creator_id int,
-	live_creator_info nvarchar(50),
-	live_creator_codename nvarchar(50),
-	live_datetime datetime2(0),
-	live_duration nvarchar(50),
-	live_prd_value int,
-	live_prd_adds int,
-	live_prds_sold int,
-	live_sku_created int,
-	live_sku_order int,
-	live_order_sold int,
-	live_numbers_cus int,
-	live_avgprice int,
-	live_clicktoorders decimal(5,2),
-	live_gmv int,
-	live_viewer_count int,
-	live_views int,
-	live_view_duration int,
-	live_cmts int,
-	live_shares int,
-	live_likes int,
-	live_followers int,
-	live_prdimpression int,
-	live_prdclick int,
-	live_ctr decimal(5,2)
+	live_creator_id nvarchar(max),
+	live_creator_info nvarchar(max),
+	live_creator_codename nvarchar(max),
+	live_datetime nvarchar(max),
+	live_duration nvarchar(max),
+	live_prd_value nvarchar(max),
+	live_prd_adds nvarchar(max),
+	live_prds_sold nvarchar(max),
+	live_sku_created nvarchar(max),
+	live_sku_order nvarchar(max),
+	live_order_sold nvarchar(max),
+	live_numbers_cus nvarchar(max),
+	live_avgprice nvarchar(max),
+	live_clicktoorders nvarchar(max),
+	live_gmv nvarchar(max),
+	live_viewer_count nvarchar(max),
+	live_views nvarchar(max),
+	live_view_duration nvarchar(max),
+	live_cmts nvarchar(max),
+	live_shares nvarchar(max),
+	live_likes nvarchar(max),
+	live_followers nvarchar(max),
+	live_prdimpression nvarchar(max),
+	live_prdclick nvarchar(max),
+	live_ctr nvarchar(max)
 );
 
 create table bronze.crm_data_videoaffiliate (
-	vid_aff_creator_info nvarchar(50),
-	vid_aff_creator_id int,
-	vid_aff_video_info nvarchar(50),
-	vid_aff_datetime datetime2(0),
-	vid_aff_prduct nvarchar(50),
-	vid_aff_views int,
-	vid_aff_likes int,
-	vid_aff_cmts int,
-	vid_aff_shares int,
-	vid_aff_followers int,
-	vid_aff_click_watchandfollow int,
-	vid_aff_productimpression int,
-	vid_aff_productclick int,
-	vid_aff_cus int,
-	vid_aff_orders int,
-	vid_aff_orderssold int,
-	vid_aff_gpm int,
-	vid_aff_gmv int,
-	vid_aff_ctr decimal(5,2),
-	vid_aff_click_watchandfollow_ratio decimal(5,2),
-	vid_aff_watchfinished decimal(5,2),
-	vid_aff_clicktoorders decimal(5,2)
+	vid_aff_creator_info nvarchar(max),
+	vid_aff_creator_id nvarchar(max),
+	vid_aff_video_info nvarchar(max),
+	vid_aff_datetime nvarchar(max),
+	vid_aff_prduct nvarchar(max),
+	vid_aff_views nvarchar(max),
+	vid_aff_likes nvarchar(max),
+	vid_aff_cmts nvarchar(max),
+	vid_aff_shares nvarchar(max),
+	vid_aff_followers nvarchar(max),
+	vid_aff_click_watchandfollow nvarchar(max),
+	vid_aff_productimpression nvarchar(max),
+	vid_aff_productclick nvarchar(max),
+	vid_aff_cus nvarchar(max),
+	vid_aff_orders nvarchar(max),
+	vid_aff_orderssold nvarchar(max),
+	vid_aff_gpm nvarchar(max),
+	vid_aff_gmv nvarchar(max),
+	vid_aff_ctr nvarchar(max),
+	vid_aff_click_watchandfollow_ratio nvarchar(max),
+	vid_aff_watchfinished nvarchar(max),
+	vid_aff_clicktoorders nvarchar(max)
 );
 
 create table bronze.crm_data_liveaffiliate (
-	live_aff_live_info nvarchar(50),
-	live_aff_live_id int,
-	live_aff_live_starttime datetime2(0),
-	live_aff_creatorname nvarchar(50),
-	live_aff_gmv int
+	live_aff_live_info nvarchar(max),
+	live_aff_live_id nvarchar(max),
+	live_aff_live_starttime nvarchar(max),
+	live_aff_creatorname nvarchar(max),
+	live_aff_gmv nvarchar(max)
 );
 
 create table bronze.crm_scheme_product (
-	prdname_name nvarchar(100),
-	prdname_id int
+	prdname_name nvarchar(max),
+	prdname_id nvarchar(max)
 );
 
 create table bronze.crm_scheme_brand (
-	brnd_channel nvarchar(50),
-	brnd_brand nvarchar(20),
-	brnd_creatorname nvarchar(50)
+	brnd_channel nvarchar(max),
+	brnd_brand nvarchar(max),
+	brnd_creatorname nvarchar(max)
 );
 
 create table bronze.crm_scheme_ecentric (
-	ecentric_channel nvarchar(50),
-	ecentric_source nvarchar(50),
-	ecentric_name nvarchar(50)
+	ecentric_channel nvarchar(max),
+	ecentric_source nvarchar(max),
+	ecentric_name nvarchar(max)
 );
 
 create table bronze.crm_scheme_ttcx (
-	ttcx_channel nvarchar(50),
-	ttcx_source nvarchar(50),
-	ttcx_name nvarchar(50)
+	ttcx_channel nvarchar(max),
+	ttcx_source nvarchar(max),
+	ttcx_name nvarchar(max)
 );
 
 create table bronze.crm_scheme_individual (
-	indvdl_name nvarchar(50),
-	indvdl_type nvarchar(50)
+	indvdl_name nvarchar(max),
+	indvdl_type nvarchar(max)
 );
 
 create table bronze.crm_scheme_brandcreator (
-	brndcrt_brand nvarchar(20),
-	brndcrt_creatorname nvarchar(50)
+	brndcrt_brand nvarchar(max),
+	brndcrt_creatorname nvarchar(max)
 );
